236efacf2cc8df70cb7716b2500dbee1af96af3e
Javier Vargas - Pc Legion <javieravargasestudiante@gmail.com>
2024-04-13 12:21:25 -0500

    Arreglo bug en build

--- src/db/firebaseConfig.ts
+++ src/db/firebaseConfig.ts
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA-lnkB3BXBlO2wNE2o-A9WN7CCtlIgSPo",
-  authDomain: "todosjavv.firebaseapp.com",
-  databaseURL: "https://todosjavv-default-rtdb.firebaseio.com",
-  projectId: "todosjavv",
-  storageBucket: "todosjavv.appspot.com",
-  messagingSenderId: "969358434948",
-  appId: "1:969358434948:web:abe348d9086972d10058d1"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA-lnkB3BXBlO2wNE2o-A9WN7CCtlIgSPo",
+//   authDomain: "todosjavv.firebaseapp.com",
+//   databaseURL: "https://todosjavv-default-rtdb.firebaseio.com",
+//   projectId: "todosjavv",
+//   storageBucket: "todosjavv.appspot.com",
+//   messagingSenderId: "969358434948",
+//   appId: "1:969358434948:web:abe348d9086972d10058d1"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
